50ecccd3ed4b3bfba75e5790e1912c74fdd5f3be
Sunqi-s <2491244476@qq.com>
2024-12-02 15:53:02 +0800

    新增保存文书年度或科技类型的相关功能，包括数据模型、控制器、服务接口及其实现、Mapper及XML配置、前端API和页面，支持基本的增删改查及导出功能。

---
+++ Archives-ui/src/views/archive/fit/index.vue
@@ -0,0 +1,255 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+<!--      <el-form-item label="存储的category_id" prop="categoryId">-->
+<!--&lt;!&ndash;        <el-input&ndash;&gt;-->
+<!--&lt;!&ndash;          v-model="queryParams.categoryId"&ndash;&gt;-->
+<!--&lt;!&ndash;          placeholder="请输入存储的category_id"&ndash;&gt;-->
+<!--&lt;!&ndash;          clearable&ndash;&gt;-->
+<!--&lt;!&ndash;          @keyup.enter.native="handleQuery"&ndash;&gt;-->
+<!--&lt;!&ndash;        />&ndash;&gt;-->
+<!--      </el-form-item>-->
+      <el-form-item label="年度" prop="syllable">
+        <el-input
+          v-model="queryParams.syllable"
+          placeholder="请输入年度"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['archive:fit:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['archive:fit:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['archive:fit:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['archive:fit:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="fitList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="id" align="center" prop="id" />
+      <el-table-column label="年度" align="center" prop="syllable" />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['archive:fit:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['archive:fit:remove']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改保存对于文书的年度或科技的类型的对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+<!--        <el-form-item label="存储的category_id" prop="categoryId">-->
+<!--          <el-input v-model="form.categoryId" placeholder="请输入存储的category_id" />-->
+<!--        </el-form-item>-->
+        <el-form-item label="年度" prop="syllable">
+          <el-input v-model="form.syllable" placeholder="请输入年度" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listFit, getFit, delFit, addFit, updateFit } from "@/api/archive/fit";
+
+export default {
+  name: "Fit",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 保存对于文书的年度或科技的类型的表格数据
+      fitList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        categoryId: 32,        syllable: null      },
+      // 表单参数
+      form: {categoryId: 32},
+      // 表单校验
+      rules: {
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询保存对于文书的年度或科技的类型的列表 */
+    getList() {
+      this.loading = true;
+      listFit(this.queryParams).then(response => {
+        this.fitList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,        categoryId: 32,        syllable: null      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加年度";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getFit(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改年度";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateFit(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            console.log(this.form)
+            addFit(this.form).then(response => {
+              console.log(response)
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$modal.confirm('是否确认删除编号为"' + ids + '"的数据项？').then(function() {
+        return delFit(ids);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('archive/fit/export', {
+        ...this.queryParams
+      }, `fit_${new Date().getTime()}.xlsx`)
+    }
+  }
+};
+</script>
---
+++ Archives/Archives-system/src/main/java/com/archives/archive/domain/ArchiveFit.java
@@ -0,0 +1,64 @@
+package com.archives.archive.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.archives.common.annotation.Excel;
+import com.archives.common.core.domain.BaseEntity;
+
+/**
+ * 保存对于文书的年度或科技的类型的对象 archive_fit
+ * 
+ * @author é«å°å·
+ * @date 2024-12-02
+ */
+public class ArchiveFit extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 年度表的id */
+    private Long id;
+
+    /** 存储的category_id */
+    private Integer categoryId;
+
+    /** 存储的字段(文书是年度、科技是类型) */
+    @Excel(name = "存储的字段(文书是年度、科技是类型)")
+    private String syllable;
+
+    public void setId(Long id) 
+    {
+        this.id = id;
+    }
+
+    public Long getId() 
+    {
+        return id;
+    }
+    public void setCategoryId(Integer categoryId) 
+    {
+        this.categoryId = categoryId;
+    }
+
+    public Integer getCategoryId() 
+    {
+        return categoryId;
+    }
+    public void setSyllable(String syllable) 
+    {
+        this.syllable = syllable;
+    }
+
+    public String getSyllable() 
+    {
+        return syllable;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+            .append("categoryId", getCategoryId())
+            .append("syllable", getSyllable())
+            .toString();
+    }
+}
