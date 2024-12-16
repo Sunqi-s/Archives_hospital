9fe08a5afefbfbfbe15a39eb583cf8613d08696a
Sunqi-s <2491244476@qq.com>
2024-12-16 14:14:43 +0800

    feat(api): 添加首页相关接口 - 新增了四个首页相关的 API 接口函数：   - getData: 获取首页数据   - getList: 获取首页列表   - getImportData: 获取首页导入数据   - getImpertOssData: 获取首页导入 OSS 数据

---
+++ Archives-ui/src/api/archive/homePage.js
@@ -0,0 +1,30 @@
+import request from '@/utils/request'
+
+export function getData(query){
+    return request({
+        url:'/home/data',
+        method:'get',
+        params:query
+    })
+}
+
+export function getList(){
+    return request({
+        url:'/home/get',
+        method:'get',
+    })
+}
+
+export function getImportData(){
+    return request({
+        url:'/home/import',
+        method:'get',
+    })
+}
+
+export function getImpertOssData(){
+    return request({
+        url:'/home/importOss',
+        method:'get',
+    })
+}
