f639f3b76cca823639619a2d371505b58d97d656
Sunqi-s <2491244476@qq.com>
2025-01-03 10:50:33 +0800

    优化档案密码输入功能 - 在密码输入框中添加 inputType='password'，确保密码隐藏显示 - 增加 inputValue=''，预设为空密码以提升用户体验

--- Archives-ui/src/views/archive/search/index.vue
+++ Archives-ui/src/views/archive/search/index.vue
@@ -369,6 +369,8 @@
           this.$prompt('请输入密码', '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
+            inputType: 'password',
+            inputValue: ''
           }).then(({ value }) => {
             if (value === tag.password) {
               this.getItemList(tag);
