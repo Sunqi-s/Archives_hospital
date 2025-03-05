a1ef4c5507b2b30f8943fd0c95012213fb771c7a
肖英豪 <1317750780@qq.com>
2025-03-05 16:14:54 +0800

    注释掉权限检查逻辑

--- Archives/Archives-system/src/main/java/com/archives/system/service/impl/SysUserServiceImpl.java
+++ Archives/Archives-system/src/main/java/com/archives/system/service/impl/SysUserServiceImpl.java
@@ -234,16 +234,16 @@ public class SysUserServiceImpl implements ISysUserService
     @Override
     public void checkUserDataScope(Long userId)
     {
-        if (!SysUser.isAdmin(SecurityUtils.getUserId()))
-        {
-            SysUser user = new SysUser();
-            user.setUserId(userId);
-            List<SysUser> users = SpringUtils.getAopProxy(this).selectUserList(user);
-            if (StringUtils.isEmpty(users))
-            {
-                throw new ServiceException("没有权限访问用户数据！");
-            }
-        }
+//        if (!SysUser.isAdmin(SecurityUtils.getUserId()))
+//        {
+//            SysUser user = new SysUser();
+//            user.setUserId(userId);
+//            List<SysUser> users = SpringUtils.getAopProxy(this).selectUserList(user);
+//            if (StringUtils.isEmpty(users))
+//            {
+//                throw new ServiceException("没有权限访问用户数据！");
+//            }
+//        }
     }
 
     /**
