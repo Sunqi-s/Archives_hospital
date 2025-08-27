5220ff8871fdb43efa45517fa17df2796eb4c953
Sunqi-s <2491244476@qq.com>
2025-08-27 11:31:47 +0800

    feat(Archives-ui):借阅记录表格添加创建时间列
    - 在 borrow index.vue 文件中添加了创建时间列
    - 新增的列显示借阅记录的创建时间，提高信息完整性

--- Archives-ui/src/views/archive/borrow/index.vue
+++ Archives-ui/src/views/archive/borrow/index.vue
@@ -127,6 +127,7 @@
           <span>{{ parseTime(scope.row.returnTime, '{y}-{m}-{d}') }}</span>
         </template>
       </el-table-column>
+      <el-table-column label="创建时间" align="center" prop="createTime" width="180" />
 <!--      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">-->
 <!--        <template slot-scope="scope">-->
 <!--          <el-button-->
